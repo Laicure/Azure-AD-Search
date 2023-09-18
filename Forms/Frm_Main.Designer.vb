dbd3f8eb265433037a8d9379f1d59e868df7f73f
LMillares <LMillares@PH-L-F4EMDSG>
2023-09-18 18:24:04 +0800

    Add project files.

---
+++ Forms/Frm_Main.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Main
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Tx_Input = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Tx_Output = New System.Windows.Forms.TextBox()
+        Me.Lb_Generate = New System.Windows.Forms.Label()
+        Me.Bg_LDAP = New System.ComponentModel.BackgroundWorker()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Lb_Generate)
+        Me.GroupBox1.Controls.Add(Me.Tx_Input)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Location = New System.Drawing.Point(1, 1)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(300, 459)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Input - Emp ID or Email per line"
+        '
+        'Tx_Input
+        '
+        Me.Tx_Input.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Tx_Input.BackColor = System.Drawing.Color.White
+        Me.Tx_Input.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Tx_Input.ForeColor = System.Drawing.Color.Black
+        Me.Tx_Input.Location = New System.Drawing.Point(3, 19)
+        Me.Tx_Input.Margin = New System.Windows.Forms.Padding(0, 0, 0, 1)
+        Me.Tx_Input.MaxLength = 0
+        Me.Tx_Input.Multiline = True
+        Me.Tx_Input.Name = "Tx_Input"
+        Me.Tx_Input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Tx_Input.Size = New System.Drawing.Size(294, 413)
+        Me.Tx_Input.TabIndex = 1
+        Me.Tx_Input.TabStop = False
+        Me.Tx_Input.WordWrap = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.Tx_Output)
+        Me.GroupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox2.Location = New System.Drawing.Point(302, 1)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(0)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(381, 459)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Output"
+        '
+        'Tx_Output
+        '
+        Me.Tx_Output.BackColor = System.Drawing.Color.White
+        Me.Tx_Output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Tx_Output.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Tx_Output.ForeColor = System.Drawing.Color.Black
+        Me.Tx_Output.Location = New System.Drawing.Point(3, 19)
+        Me.Tx_Output.MaxLength = 0
+        Me.Tx_Output.Multiline = True
+        Me.Tx_Output.Name = "Tx_Output"
+        Me.Tx_Output.ReadOnly = True
+        Me.Tx_Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Tx_Output.Size = New System.Drawing.Size(375, 437)
+        Me.Tx_Output.TabIndex = 2
+        Me.Tx_Output.TabStop = False
+        Me.Tx_Output.WordWrap = False
+        '
+        'Lb_Generate
+        '
+        Me.Lb_Generate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Lb_Generate.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Lb_Generate.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Lb_Generate.Enabled = False
+        Me.Lb_Generate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Lb_Generate.ForeColor = System.Drawing.Color.Blue
+        Me.Lb_Generate.Location = New System.Drawing.Point(3, 433)
+        Me.Lb_Generate.Margin = New System.Windows.Forms.Padding(0)
+        Me.Lb_Generate.Name = "Lb_Generate"
+        Me.Lb_Generate.Size = New System.Drawing.Size(294, 23)
+        Me.Lb_Generate.TabIndex = 3
+        Me.Lb_Generate.Text = "Search Counterpart"
+        Me.Lb_Generate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Bg_LDAP
+        '
+        '
+        'Frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(684, 461)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(700, 500)
+        Me.MinimumSize = New System.Drawing.Size(700, 500)
+        Me.Name = "Frm_Main"
+        Me.Padding = New System.Windows.Forms.Padding(1)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Frm_Main"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Tx_Input As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Tx_Output As TextBox
+    Friend WithEvents Lb_Generate As Label
+    Friend WithEvents Bg_LDAP As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Forms/Frm_Main.vb
@@ -0,0 +1,128 @@
+﻿Imports System.DirectoryServices
+Imports System.Globalization
+
+Public Class Frm_Main
+
+	Dim inputLines As New HashSet(Of String)
+	Dim outputLines As New HashSet(Of String)
+	Dim errx() As String = {}
+	Dim startExec As DateTime = DateTime.UtcNow
+	Dim glob As CultureInfo = CultureInfo.InvariantCulture
+
+	Private Sub Frm_Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		With Me
+			.Text = "Azure AD Search v" & My.Application.Info.Version.ToString
+			.Icon = My.Resources.Icon1
+		End With
+
+	End Sub
+
+	Private Sub Frm_Main_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+		If MessageBox.Show("Are you sure to close the Azure AD Search app?", "Close?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.No Then e.Cancel = True
+	End Sub
+
+	Private Sub Tx_Input_TextChanged(sender As Object, e As EventArgs) Handles Tx_Input.TextChanged
+		inputLines = Tx_Input.Lines.Where(Function(x) Int64.TryParse(x, Nothing) OrElse x.Contains("@")).Select(Function(x) x.Replace(" ", "")).ToHashSet
+		With Lb_Generate
+			.Enabled = Not inputLines.LongCount = 0
+			.Text = "Search Counterpart"
+		End With
+	End Sub
+
+	Private Sub Lb_Generate_Click(sender As Object, e As EventArgs) Handles Lb_Generate.Click
+		If Not Bg_LDAP.IsBusy Then
+			startExec = DateTime.UtcNow
+
+			Tx_Input.ReadOnly = True
+			Tx_Output.Clear()
+			With Lb_Generate
+				.Enabled = False
+				.Text = "Searching..."
+			End With
+
+			Bg_LDAP.RunWorkerAsync()
+		End If
+
+	End Sub
+
+#Region "Background Worker"
+
+	Private Sub Bg_LDAP_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles Bg_LDAP.DoWork
+		Try
+			'@@@@@@ search by mail or employee number
+			For Each str As String In inputLines
+				Using adSearcher As New DirectorySearcher
+					With adSearcher
+						If Int64.TryParse(str, Nothing) Then
+							.Filter = "employeeNumber=" & str
+							.PropertiesToLoad.Add("mail")
+						Else
+							.Filter = "mail=" & str
+							.PropertiesToLoad.Add("employeeNumber")
+						End If
+
+						Dim resul As SearchResult = .FindOne
+						If Not IsNothing(resul) Then
+							Dim user As DirectoryEntry = resul.GetDirectoryEntry
+
+							If Int64.TryParse(str, Nothing) Then
+								outputLines.Add(user.Properties("mail").Value.ToString.ToLowerInvariant)
+							Else
+								outputLines.Add(user.Properties("employeeNumber").Value.ToString)
+							End If
+						Else
+							outputLines.Add("-")
+						End If
+					End With
+				End Using
+			Next
+
+			'@@@@@@ search by Identity
+			'Using ctx As New PrincipalContext(ContextType.Domain, "ap.cbre.net")
+			'	Dim userPrin As UserPrincipal = UserPrincipal.FindByIdentity(ctx, IdentityType.SamAccountName, "LMillares")
+			'	If Not IsNothing(userPrin) Then outputLines.Add(userPrin.Description)
+			'End Using
+
+			'@@@@@@ search all
+			'Using ctx As New PrincipalContext(ContextType.Domain, "ap.cbre.net")
+			'	Using userPrin As New UserPrincipal(ctx)
+			'		Using sear As New PrincipalSearcher(userPrin)
+			'			For Each domUser As Principal In sear.FindAll
+			'				outputLines.Add(domUser.UserPrincipalName & " | " & domUser.SamAccountName & " | " & domUser.DisplayName & " | " & domUser.DistinguishedName & " | " & domUser.Name & " | " & domUser.SamAccountName & " | " & domUser.Sid.Value)
+			'			Next
+			'		End Using
+			'	End Using
+			'End Using
+
+			errx = {}
+		Catch ex As Exception
+			errx = {Err.Description, Err.Source}
+		End Try
+	End Sub
+
+	Private Sub Bg_LDAP_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles Bg_LDAP.RunWorkerCompleted
+		Dim timeCompleted As String = Microsoft.VisualBasic.Left(DateTime.UtcNow.Subtract(startExec).ToString, 11)
+		If errx.Count = 2 Then
+			Lb_Generate.Text = "Error @ " & timeCompleted
+			MessageBox.Show(errx(0), errx(1), MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+		Else
+			Tx_Output.Lines = outputLines.ToArray
+			Lb_Generate.Text = "Done @ " & timeCompleted
+
+			MessageBox.Show("Took " & timeCompleted, "Successfully Generated!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+			Tx_Output.Focus()
+		End If
+
+		Tx_Input.ReadOnly = False
+		Lb_Generate.Enabled = True
+
+		With outputLines
+			.Clear()
+			.TrimExcess()
+		End With
+	End Sub
+
+#End Region
+
+End Class
